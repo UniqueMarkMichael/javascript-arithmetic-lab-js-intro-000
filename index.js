b569c9c7ee574af26e8d374691b6765c1ac4f446
Unique Mark Michael <uniquemarkmichael@gmail.com>
2020-03-26 13:03:52 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function add(a,b) {return a + b}
+function subtract(a,b) {return a - b}
+function multiply(a,b) {return a * b}
+function divide(a,b) {return a / b}
+
+function increment(var1){return var1 + 1}
+function decrement(var1){return var1 - 1}
+
+
+function makeInt(n) {
+ parseInt("n",10)
+ return (n)
+}
+
+
+
+function preserveDecimal(n){
+ parseFloat('n')
+ return (n)
+}
